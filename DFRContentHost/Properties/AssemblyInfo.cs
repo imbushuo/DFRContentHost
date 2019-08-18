0e73c51c2aa2b27836427ae9401b3b4284545f3a
Bingxing Wang <binwang@linkedin.com>
2019-08-18 13:25:52 -0700

    Use AssemblyInfo

---
+++ DFRContentHost/Properties/AssemblyInfo.cs
@@ -0,0 +1,29 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Touch Bar Content Host Application")]
+[assembly: AssemblyDescription("Touch Bar Content Host Application")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("DFRContentHost Authors")]
+[assembly: AssemblyProduct("Touch Bar Content Host")]
+[assembly: AssemblyCopyright("Copyright © Bingxing Wang 2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("482a2787-2340-443e-b766-62d964bf9150")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
